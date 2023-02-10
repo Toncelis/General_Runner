87a935a504c831c2c98a9d6e5c90c7e31cc507b1
Антон Морозов <vedavonza@gmail.com>
2023-02-10 18:01:45 +0300

    Заготовка генерации меша тайла

--- Assets/Scripts/EditorTools/SplineMesh/SplineMeshCreator.cs
+++ Assets/Scripts/EditorTools/SplineMesh/SplineMeshCreator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using DefaultNamespace.EditorTools.Splines;
 using Extensions;
 using Sirenix.OdinInspector;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
@@ -12,9 +13,9 @@ public class SplineMeshCreator : MonoBehaviour {
     public Material RoadMaterial;
     public int Density;
     public float RoadWidth;
-
+    
     [Button]
-    public void GenerateMesh() {
+    public void GenerateMesh(float leftEdgeOffset, float rightEdgeOffset, float bottomEdgeOffset, float topEdgeOffset, MeshFilter meshFilter) {
         var spline = SplineCreator.Spline;
         if (spline.count < 2) {
             return;
@@ -24,43 +25,82 @@ public class SplineMeshCreator : MonoBehaviour {
             name = "Procedural Mesh"
         };
 
-        var vertices = GenerateVertices(spline);
+        var vertices = GenerateVertices(spline, leftEdgeOffset, rightEdgeOffset, bottomEdgeOffset, topEdgeOffset);
         mesh.vertices = vertices;
 
         var triangles = GenerateTrianglesWithVolume(vertices.Length);
         mesh.triangles = triangles.ToArray();
 
-        GetComponent<MeshFilter>().mesh = mesh;
-        GetComponent<MeshRenderer>().material = RoadMaterial;
+        meshFilter.mesh = mesh;
     }
 
-    private Vector3[] GenerateVertices(Spline spline) {
+    [Button]
+    public GameObject GenerateChildMesh(string childName, float leftEdgeOffset, float rightEdgeOffset, float bottomEdgeOffset, float topEdgeOffset) {
+        MeshFilter meshFilter;
+        
+        for (int i = 0; i < transform.childCount; i++) {
+            var child = transform.GetChild(i);
+            if (child.name != childName) {
+                continue;
+            }
+            
+            meshFilter = child.GetOrAddComponent<MeshFilter>();
+            GenerateMesh(leftEdgeOffset, rightEdgeOffset, bottomEdgeOffset, topEdgeOffset, meshFilter);
+            return child.gameObject;
+        }
+
+        var meshHolder = new GameObject(childName);
+        meshHolder.transform.parent = transform;
+        meshHolder.transform.localPosition = Vector3.zero;
+        meshFilter = meshHolder.AddComponent<MeshFilter>();
+        GenerateMesh(leftEdgeOffset, rightEdgeOffset, bottomEdgeOffset, topEdgeOffset, meshFilter);
+        return meshHolder;
+    }
+
+    [Button]
+    public void GenerateRoadWithWalls(float roadWidth, float wallWidth, float wallHeight) {
+        float roadHalfWidth = roadWidth/2;
+        var road = GenerateChildMesh("Road", -roadHalfWidth, roadHalfWidth, -2, 0);
+        road.tag = "ground";
+        road.GetOrAddComponent<MeshCollider>();
+        road.GetOrAddComponent<MeshRenderer>();
+        var leftWall = GenerateChildMesh("LeftWall", -wallWidth-roadHalfWidth, -roadHalfWidth, -2, wallHeight);
+        leftWall.tag = "Wall";
+        leftWall.GetOrAddComponent<MeshCollider>();
+        leftWall.GetOrAddComponent<MeshRenderer>();
+        var rightWall = GenerateChildMesh("RightWall", roadHalfWidth, wallWidth+roadHalfWidth, -2, wallHeight);
+        rightWall.tag = "Wall";
+        rightWall.GetOrAddComponent<MeshCollider>();
+        rightWall.GetOrAddComponent<MeshRenderer>();
+    }
+
+    private Vector3[] GenerateVertices(Spline spline, float leftEdgeOffset, float rightEdgeOffset, float bottomEdgeOffset, float topEdgeOffset) {
         var vertices = new List<Vector3>();
 
         var segmentStart = spline.points[0].Position;
-        AddSquareVertices(segmentStart, spline.points[0].Direction, vertices, RoadWidth);
+        AddSquareVertices(segmentStart, spline.points[0].Direction, vertices, leftEdgeOffset, rightEdgeOffset, bottomEdgeOffset, topEdgeOffset);
 
         float step = 1f / Density;
 
         for (int i = 0; i < spline.count - 1; i++) {
             for (float t = step; t <= 1; t += step) {
                 var segmentEnd = spline.Bezier(i, t);
-                AddSquareVertices(segmentEnd, segmentEnd - segmentStart, vertices, RoadWidth);
+                AddSquareVertices(segmentEnd, segmentEnd - segmentStart, vertices, leftEdgeOffset, rightEdgeOffset, bottomEdgeOffset, topEdgeOffset);
                 segmentStart = segmentEnd;
             }
         }
-        AddSquareVertices(spline.points.Last().Position, spline.points.Last().Direction, vertices, RoadWidth);
+        AddSquareVertices(spline.points.Last().Position, spline.points.Last().Direction, vertices, leftEdgeOffset, rightEdgeOffset, bottomEdgeOffset, topEdgeOffset);
 
         return vertices.ToArray();
     }
 
-    private void AddSquareVertices(Vector3 center, Vector3 direction, List<Vector3> vertices, float width, float height = 4) {
-        direction = direction.WithY(0).normalized * width;
+    private void AddSquareVertices(Vector3 center, Vector3 direction, List<Vector3> vertices, float leftEdgeOffset, float rightEdgeOffset, float bottomEdgeOffset, float topEdgeOffset) {
+        direction = direction.WithY(0).normalized;
         var sideVector = Quaternion.Euler(0, 90, 0) * direction;
-        vertices.Add(center + sideVector);
-        vertices.Add(center - sideVector);
-        vertices.Add(center + sideVector + Vector3.down * height);
-        vertices.Add(center - sideVector + Vector3.down * height);
+        vertices.Add(center + sideVector * rightEdgeOffset + Vector3.up * topEdgeOffset);
+        vertices.Add(center + sideVector * leftEdgeOffset + Vector3.up * topEdgeOffset);
+        vertices.Add(center + sideVector * rightEdgeOffset + Vector3.up * bottomEdgeOffset);
+        vertices.Add(center + sideVector * leftEdgeOffset + Vector3.up * bottomEdgeOffset);
     }
 
     private int[] GenerateTrianglesWithVolume(int verticesCount) {
---
+++ Assets/Scripts/Extensions/GameObjectExt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Extensions {
+    public static class GameObjectExt {
+        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component {
+            var component = gameObject.GetComponent<T>();
+            if (component == null) {
+                component = gameObject.AddComponent<T>();
+            }
+            return component;
+        }
+    }
+}
